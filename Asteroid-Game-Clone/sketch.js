efac4ed9d1ee6ab3773c44307309aeb6c6ab47dd
Gregory Eales <gregoeales@aol.com>
2020-04-21 15:21:07 +0100

    Update sketch.js

--- Asteroid-Game-Clone/sketch.js
+++ Asteroid-Game-Clone/sketch.js
@@ -50,24 +50,54 @@ function checkCollisions(spaceship, asteroids){
 
     //spaceship-2-asteroid collisions
     //YOUR CODE HERE (2-3 lines approx)
+    for(var i=0; i<asteroids.locations.length; i++){
+      if(isInside(spaceship.location, spaceship.size, asteroids.locations[i], asteroids.diams[i])){
+        gameOver()
+      }
+    }
 
     //asteroid-2-earth collisions
     //YOUR CODE HERE (2-3 lines approx)
+    for(var i=0; i<asteroids.locations.length; i++){
+      if(isInside(earthLoc, earthSize.x, asteroids.locations[i], asteroids.diams[i])){
+        gameOver()
+      }
+    }
 
     //spaceship-2-earth
     //YOUR CODE HERE (1-2 lines approx)
+    if(isInside(earthLoc, earthSize.x, spaceship.location, spaceship.size)){
+        gameOver()
+      }
 
     //spaceship-2-atmosphere
     //YOUR CODE HERE (1-2 lines approx)
+    if(isInside(atmosphereLoc, atmosphereSize.x, spaceship.location, spaceship.size)){
+        spaceship.setNearEarth()
+      }
 
     //bullet collisions
     //YOUR CODE HERE (3-4 lines approx)
+    for(var i=0; i<asteroids.locations.length; i++){
+      for(var j=0; j<spaceship.bulletSys.bullets.length; j++){
+        if(isInside(spaceship.bulletSys.bullets[j], spaceship.bulletSys.diam, asteroids.locations[i], asteroids.diams[i])){
+          asteroids.destroy(i)
+          spaceship.bulletSys.bullets.splice(j, 1)
+        }
+      }
+    }
 }
 
 //////////////////////////////////////////////////
 //helper function checking if there's collision between object A and object B
 function isInside(locA, sizeA, locB, sizeB){
     // YOUR CODE HERE (3-5 lines approx)
+    if(abs(locA.x - locB.x) < sizeA/2 + sizeB/2){
+      if(abs(locA.y - locB.y) < sizeA/2 + sizeB/2){
+        return true;
+      }
+    }
+    return false;
 }
 
 //////////////////////////////////////////////////
