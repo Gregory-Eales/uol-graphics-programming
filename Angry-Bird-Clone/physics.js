80834bb4b8291f322edca8eabd8f979701b27c9f
Gregory Eales <gregoeales@aol.com>
2020-04-21 23:57:41 +0100

    Update physics.js

--- Angry-Bird-Clone/physics.js
+++ Angry-Bird-Clone/physics.js
@@ -15,13 +15,19 @@ function drawGround(){
 }
 ////////////////////////////////////////////////////////////////
 function setupPropeller(){
-  // your code here
+  propeller = Bodies.rectangle(150, 480, 200, 15, {isStatic:true, angle:angle});
+  World.add(engine.world, [propeller]);
+
 }
 ////////////////////////////////////////////////////////////////
 //updates and draws the propeller
 function drawPropeller(){
   push();
-  // your code here
+  fill(128);
+  angle += angleSpeed
+  Body.setAngle(propeller, angle);
+  Body.setAngularVelocity(propeller, angleSpeed);
+  drawVertices(propeller.vertices);
   pop();
 }
 ////////////////////////////////////////////////////////////////
@@ -35,30 +41,72 @@ function setupBird(){
 ////////////////////////////////////////////////////////////////
 function drawBirds(){
   push();
-  //your code here
+  for(var i=0; i<birds.length; i++)
+  {
+    
+    if(isOffScreen(birds[i]) )
+    {
+      removeFromWorld(birds[i]);
+      birds.splice(i, 1)
+      i-=1;
+    }
+
+    else
+    {
+      drawVertices(birds[i].vertices)
+    }
+
+  }
   pop();
 }
 ////////////////////////////////////////////////////////////////
 //creates a tower of boxes
 function setupTower(){
   //you code here
+  for(var i=0; i<6; i++)
+  {
+    for(var j=0; j<3; j++)
+    { 
+      colors.push(color(0, random(150,255), 0));
+      boxes.push(Bodies.rectangle(750+j*80, 0, 80, 80));
+    }
+  }
+  World.add(engine.world, boxes);
 }
 ////////////////////////////////////////////////////////////////
 //draws tower of boxes
 function drawTower(){
   push();
-  //your code here
+  for(var i=0; i<18; i++)
+  { 
+    fill(colors[i]);
+    drawVertices(boxes[i].vertices);
+  }
   pop();
 }
 ////////////////////////////////////////////////////////////////
 function setupSlingshot(){
 //your code here
+slingshotBird = Bodies.circle(150, 200, 20, {friction: 0, restitution: 0.95 });
+Matter.Body.setMass(slingshotBird , slingshotBird.mass*10);
+
+var options = {bodyB:slingshotBird, pointA:{x:150, y:200}, stiffness:0.01, length:10,
+ damping:0.0001}
+slingshotConstraint = Constraint.create(options);
+
+
+
+
+World.add(engine.world, [slingshotBird, slingshotConstraint]);
 }
 ////////////////////////////////////////////////////////////////
 //draws slingshot bird and its constraint
 function drawSlingshot(){
   push();
   // your code here
+  fill(255, 255, 255);
+  drawVertices(slingshotBird.vertices);
+  drawConstraint(slingshotConstraint);
   pop();
 }
 /////////////////////////////////////////////////////////////////
